250ea1e80b1fc3b28016dfaa51d9457ff6d8d303
Lucas_Gentile <lucasgpc@gmail.com>
2017-12-26 15:25:05 -0200

    Fix config.js

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test/e2e/tests/*.js'],
-    capabilities: {
-        'browserName': 'chrome',
-        'chromeOptions': { 
-            'args': [
-	        '--disable-infobars', '--headless', '--disable-gpu', '--window-size=800,600'
-            ]
-        }
-    }	
+    specs: ['test/e2e/tests/*.js']
 }
